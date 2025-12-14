1b54dff4332183ba318c0cb537d4fa6558a69afd
校奥浆糊 <wst22@mails.tsinghua.edu.cn>
2025-12-14 15:25:19 +0800

    Add new friend entry with title and avatar

--- friends.js
+++ friends.js
@@ -5,4 +5,9 @@ export default [
     avatarUrl: "https://blog.erroro.moe/assets/avatar.jpg",
     link: "https://blog.erroro.moe/",
   },
+  {
+    title: "校奥浆糊",
+    avatarUrl: "https://blog.xiaoaojianghu.fun/img/icon.jpeg",
+    link: "https://blog.xiaoaojianghu.fun/",
+  },
 ];
